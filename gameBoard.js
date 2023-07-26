15ec177a3fedec51b5bb816952bb8dd78644e3ef
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-07-26 15:38:23 +0800

    gameBoard .1

---
+++ gameBoard.js
@@ -0,0 +1,107 @@
+import { createShip } from './ship'
+export const gameBoard = () => {
+  const createGameBoard10X10 = () => {
+    let board = []
+    for (let i = 1; i < 11; i++) {
+      for (let j = 1; j < 11; j++) {
+        board.push([i, j])
+      }
+    }
+    return board
+  }
+  const gb = createGameBoard10X10()
+
+  // No.	Class of ship	Size
+  // 1	Carrier	5
+  // 2	Battleship	4
+  // 3	Destroyer	3
+  // 4	Submarine	3
+  // 5	Patrol Boat	2
+  let takenbyship = []
+  let allShip = {}
+  const placeShip = (array, name) => {
+    //check taken or not
+    for (const cell of array) {
+      const arrx = cell[0]
+      const arry = cell[1]
+      for (const cellt of takenbyship) {
+        const tx = cellt[0]
+        const ty = cellt[1]
+        if (arrx === tx && arry === ty) {
+          return 'duplicate place'
+        }
+      }
+    }
+    takenbyship.push(...array)
+
+    if (name === 'carrier' && !allShip.carrier) {
+      let carrier = createShip(5)
+      carrier.pos = array
+      allShip.carrier = carrier
+      return
+    } else if (name === 'battleship' && !allShip.battleship) {
+      let battleship = createShip(4)
+      battleship.pos = array
+      allShip.battleship = battleship
+    } else if (name === 'destoroyer' && !allShip.destoroyer) {
+      let destoroyer = createShip(3)
+      destoroyer.pos = array
+      allShip.destoroyer = destoroyer
+    } else if (name === 'submarine' && !allShip.submarine) {
+      let submarine = createShip(3)
+      submarine.pos = array
+      allShip.submarine = submarine
+    } else if (name === 'patrolBoat' && !allShip.patrolBoat) {
+      let patrolBoat = createShip(2)
+      patrolBoat.pos = array
+      allShip.patrolBoat = patrolBoat
+    }
+  }
+
+  let hitPos = []
+  let missPos = []
+  const receiveAttack = pos => {
+    //check if receive same pos attack
+    const posx = pos[0]
+    const posy = pos[1]
+    if (hitPos) {
+      for (const hit of hitPos) {
+        const [hitx, hity] = hit
+        if (hitx === posx && hity === posy) {
+          return 'same pos'
+        }
+      }
+    }
+    if (missPos) {
+      for (const miss of missPos) {
+        const [missx, missy] = miss
+        if (missx === posx && missy === posy) {
+          return 'same pos'
+        }
+      }
+    }
+    const carrier = allShip.carrier
+    const battleship = allShip.battleship
+    const destroyer = allShip.battleship
+    const submarine = allShip.submarine
+    const patrolBoat = allShip.patrolBoat
+    const boatarray = [carrier, battleship, destroyer, submarine, patrolBoat]
+    for (const boat of boatarray) {
+      if (boat && boat.pos && Array.isArray(boat.pos)) {
+        for (const cell of boat.pos) {
+          const arrx = cell[0]
+          const arry = cell[1]
+          if (arrx === posx && arry === posy) {
+            const index = boat.pos.indexOf(cell)
+            boat.pos.splice(index, 1)
+            boat.hit()
+            hitPos.push(pos)
+            return
+          }
+        }
+      }
+    }
+    missPos.push(pos)
+  }
+  return { placeShip, allShip, receiveAttack, hitPos, missPos }
+}
---
+++ gameBoard.test.js
@@ -0,0 +1,121 @@
+import { gameBoard } from './gameBoard'
+import { createShip } from './ship'
+test.todo('10*10grid')
+
+test('placeship function', () => {
+  const gb = gameBoard()
+  gb.placeShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5]
+    ],
+    'carrier'
+  )
+  expect(gb.allShip.carrier.pos).toEqual([
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [1, 5]
+  ])
+})
+test('if pos is taken by other ship', () => {
+  const gb = gameBoard()
+  const carrier = gb.placeShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5]
+    ],
+    'carrier'
+  )
+  const battleship = gb.placeShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4]
+    ],
+    'battleship'
+  )
+  expect(battleship).toBe('duplicate place')
+})
+test('receiveAttack function', () => {
+  const gb = gameBoard()
+  gb.placeShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5]
+    ],
+    'carrier'
+  )
+  gb.receiveAttack([1, 1])
+  expect(gb.allShip.carrier.pos).toEqual([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [1, 5]
+  ])
+})
+
+test('receiveAttack ,hit or miss', () => {
+  const gb = gameBoard()
+  gb.placeShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5]
+    ],
+    'carrier'
+  )
+  gb.receiveAttack([1, 1])
+  gb.receiveAttack([2, 2])
+  gb.receiveAttack([3, 3])
+
+  expect(gb.hitPos).toEqual([[1, 1]])
+  expect(gb.missPos).toEqual([
+    [2, 2],
+    [3, 3]
+  ])
+})
+test('if receive same attack pos', () => {
+  const gb = gameBoard()
+  gb.placeShip(
+    [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5]
+    ],
+    'carrier'
+  )
+  gb.receiveAttack([3, 3])
+  expect(gb.missPos).toEqual([[3, 3]])
+  const t = gb.receiveAttack([3, 3])
+  expect(t).toBe('same pos')
+})
+
+test('hit and issunk function in ship.js', () => {
+  const gb = gameBoard()
+  gb.placeShip(
+    [
+      [1, 1],
+      [2, 1]
+    ],
+    'patrolBoat'
+  )
+  gb.receiveAttack([1, 1])
+  gb.receiveAttack([2, 1])
+  expect(gb.allShip.patrolBoat.isSunk()).toBe(true)
+})
