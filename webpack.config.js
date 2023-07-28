628ea996fce2141c886e18b790d765fad9783b8e
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-07-28 21:50:12 +0800

    webpack adjust

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/gameFlow.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   }
 }
+//note: to use webpack
+//1. npm install webpack webpack-cli --save-dev
+//2. in package.json add script "build": "webpack"
+//3. create webpack.config.js  and  add the code above
+//4. create src folder and put all needed js in
+//5. check the path is correct
+//6. npm run build  in terminal , to create bundle.js
